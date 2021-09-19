f812bc70fffeb8a3713b634642c27f7e08d946de
Olakunle Arewa <arewa.olakunle@gmail.com>
2021-09-19 22:28:25 +0100

    test: tests for the redis store

--- test/tokens/redis.store.spec.ts
+++ test/tokens/redis.store.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import faker from "faker";
 import IORedis, { Redis } from "ioredis";
+import { after } from "mocha";
 import { RedisStore } from "../../src";
 import { randomString, timeout } from "../helpers";
 
@@ -12,6 +13,10 @@ before(async () => {
 
   redis = new IORedis();
   store = new RedisStore(secret, redis);
+});
+
+after(async () => {
+  await redis.quit();
 });
 
 afterEach(async () => {
@@ -28,11 +33,9 @@ describe("RedisStore#commission", () => {
 
   it("should create a token that expires after 100ms", async () => {
     const token = await store.commision(faker.internet.userName(), faker.internet.email(), "100ms");
+    const t = await redis.pttl(token);
 
-    await timeout("110ms");
-    const val = await redis.get(token);
-
-    expect(val).to.be.null;
+    expect(t).to.lessThanOrEqual(100);
   });
 
   it("should create same token for the same key", async () => {
@@ -53,6 +56,7 @@ describe("RedisStore#peek", () => {
 
     expect(value).to.equal(data);
     expect(val).to.be.a("string");
+    expect(val).to.not.be.empty;
   });
 
   it("should return null after the token expires", async () => {
@@ -60,6 +64,88 @@ describe("RedisStore#peek", () => {
 
     await timeout("110ms");
     const val = await store.peek(token);
+
+    expect(val).to.be.null;
+  });
+});
+
+describe("RedisStore#extend", () => {
+  it("should change the lifetime of a token", async () => {
+    const data = faker.internet.email();
+    const token = await store.commision(faker.internet.userName(), data, "300ms");
+    const oldTTL = await redis.pttl(token);
+
+    const fromExtraction = await store.extend(token, "200ms");
+    const newTTL = await redis.pttl(token);
+
+    expect(fromExtraction).to.equal(data);
+    expect(newTTL).to.not.eq(oldTTL);
+    expect(newTTL).to.lessThanOrEqual(200);
+  });
+
+  it("should return null after the token expires", async () => {
+    const token = await store.commision(faker.internet.userName(), faker.internet.email(), "100ms");
+
+    await timeout("110ms");
+    const val = await store.extend(token, "10ms");
+
+    expect(val).to.be.null;
+  });
+});
+
+describe("RedisStore#reset", () => {
+  it("should reset data a token represents", async () => {
+    const data = faker.internet.email();
+    const key = faker.internet.userName();
+    const token = await store.commision(key, data, "100ms");
+    const newData = faker.internet.email();
+
+    await store.reset(key, newData);
+    const newValue = await store.peek(token);
+
+    expect(newValue).to.equal(newData);
+    expect(newValue).not.eq(data);
+  });
+
+  it("should not change the timeout on the token", async () => {
+    const key = faker.internet.userName();
+    const token = await store.commision(key, faker.internet.email(), "100ms");
+
+    await timeout("50ms");
+    await store.reset(key, faker.internet.email());
+    const newTTL = await redis.pttl(token);
+
+    expect(newTTL).to.lessThanOrEqual(50);
+  });
+});
+
+describe("RedisStore#decommission", () => {
+  it("should return a value and destroy the token", async () => {
+    const data = faker.internet.email();
+    const token = await store.commision(faker.internet.userName(), data, "3s");
+    const value = await store.decommission(token);
+    const val = await redis.get(token);
+
+    expect(value).to.equal(data);
+    expect(val).to.be.null;
+  });
+
+  it("should return null after the token expires", async () => {
+    const token = await store.commision(faker.internet.userName(), faker.internet.email(), "100ms");
+
+    await timeout("110ms");
+    const val = await store.decommission(token);
+
+    expect(val).to.be.null;
+  });
+});
+
+describe("RedisStore#revoke", () => {
+  it("should delete token for a key if it exists", async () => {
+    const key = faker.internet.userName();
+    const token = await store.commision(key, faker.internet.email(), "3s");
+    await store.revoke(key);
+    const val = await redis.get(token);
 
     expect(val).to.be.null;
   });
