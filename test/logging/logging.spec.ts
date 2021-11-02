b63aae420a236f66d54d8557df6d1a892667c9cc
kassim <alkassimk@gmail.com>
2021-11-02 16:02:31 +0100

    test: update tests

--- test/logging/logging.spec.ts
+++ test/logging/logging.spec.ts
@@ -1,119 +1,72 @@
-import { Logger } from "../../src/logging";
-import nock from "nock";
+import { Logger, defaultSerializers, createRequestSerializer } from "../../src/logging";
 import { expect } from "chai";
 import axios from "axios";
+import app from "./server";
+import http from "http";
 
-const logger = new Logger("test", "dev", {});
-const interceptUrl = "https://api.github.com/repos/atom/atom/license";
-const errorInterceptUrl = "http://www.google.com/cat-poems";
+export const logger = new Logger(
+  "logger_tests",
+  "dev",
+  defaultSerializers(
+    {
+      http_req: createRequestSerializer("admin.password")
+    },
+    "password"
+  )
+);
 
-//to test successful requests
-nock("https://api.github.com")
-  .get("/repos/atom/atom/license")
-  .times(2)
-  .reply(200, {
-    license: {
-      key: "mit",
-      name: "MIT License",
-      spdx_id: "MIT",
-      url: "https://api.github.com/licenses/mit",
-      node_id: "MDc6TGljZW5zZTEz"
-    }
-  });
+const baseUrl = "http://localhost:3005";
 
-//to test requests that throw an error
-nock("http://www.google.com").get("/cat-poems").replyWithError("something awful happened");
+before(() => {
+  http.createServer(app).listen(3005);
+});
 
 // clears the buffer after each test
 afterEach(() => {
   logger.ringbuffer.records = [];
 });
 
-describe("Bunyan#Log", () => {
-  it("should be able to log and write multiple messages to buffer", () => {
-    logger.log("hello world");
-    logger.log("hello world");
-
-    expect(logger.ringbuffer.records).to.be.length(2);
-  });
-
-  it("returns the correct object", () => {
-    logger.log("hello world");
-    const keys = Object.keys(JSON.parse(logger.ringbuffer.records[0]));
-
-    const success = keys.every(function (val) {
-      return ["name", "hostname", "pid", "level", "msg", "time", "v"].indexOf(val) !== -1;
-    });
-
-    expect(success).to.be.true;
-  });
-});
-
-describe("Bunyan#Error", () => {
-  it("should log the correct error message", () => {
-    logger.error("this is an internal application error");
-
-    const errorObject = JSON.parse(logger.ringbuffer.records[0]);
-
-    expect(logger.ringbuffer.records).to.be.length(1);
-    expect(errorObject.msg).to.be.equal("this is an internal application error");
-  });
-});
-
 describe("Bunyan#Request", () => {
   it("should be able to log a request", async () => {
-    logger.request(await axios.get(interceptUrl));
+    await axios.get(`${baseUrl}/req`);
 
-    const keys = Object.keys(JSON.parse(logger.ringbuffer.records[0]));
+    const properties = JSON.parse(logger.ringbuffer.records);
 
-    const success = ["name", "hostname", "pid", "level", "msg", "time", "v", "req"].every(function (val) {
-      return keys.indexOf(val) !== -1;
-    });
-
-    const log = JSON.parse(logger.ringbuffer.records[0]);
-
-    expect(success).to.be.true;
-    expect(log.req.status).to.equal(200);
-    expect(log.req.config.url).to.equal(interceptUrl);
     expect(logger.ringbuffer.records).to.be.length(1);
+    expect(properties.req).to.have.property("method");
+    expect(properties.req).to.have.property("url");
+    expect(properties.req).to.have.property("headers");
+    expect(properties.req).to.have.property("remoteAddress");
+    expect(properties.req).to.have.property("remotePort");
   });
 });
 
 describe("Bunyan#Response", () => {
   it("should be able to log a response", async () => {
-    const data = await axios.get(interceptUrl);
+    await axios.get(`${baseUrl}/req-res`);
 
-    logger.response(data.request, data.data);
+    const properties = JSON.parse(logger.ringbuffer.records);
 
-    const keys = Object.keys(JSON.parse(logger.ringbuffer.records[0]));
-
-    const success = ["name", "hostname", "pid", "level", "msg", "time", "v", "req", "res"].every(function (val) {
-      return keys.indexOf(val) !== -1;
-    });
-
-    expect(success).to.be.true;
     expect(logger.ringbuffer.records).to.be.length(1);
+    expect(properties.req).to.have.property("method");
+    expect(properties.req).to.have.property("url");
+    expect(properties.req).to.have.property("headers");
+    expect(properties.req).to.have.property("remoteAddress");
+    expect(properties.req).to.have.property("remotePort");
   });
 });
 
 describe("Bunyan#httpError", () => {
   it("should be able to log a http error", async () => {
-    try {
-      await axios.get(errorInterceptUrl);
-    } catch (error) {
-      logger.httpError(error, error.request, error.response);
+    await axios.get(`${baseUrl}/error`);
 
-      const keys = Object.keys(JSON.parse(logger.ringbuffer.records[0]));
+    const properties = JSON.parse(logger.ringbuffer.records);
 
-      const success = ["name", "hostname", "pid", "level", "msg", "time", "v", "req", "err"].every(function (val) {
-        return keys.indexOf(val) !== -1;
-      });
-
-      const log = JSON.parse(logger.ringbuffer.records[0]);
-
-      expect(success).to.be.true;
-      expect(log.err.message).to.be.equal("something awful happened");
-      expect(log.err.config.url).to.be.equal(errorInterceptUrl);
-    }
+    expect(logger.ringbuffer.records).to.be.length(1);
+    expect(properties.req).to.have.property("method");
+    expect(properties.req).to.have.property("url");
+    expect(properties.req).to.have.property("headers");
+    expect(properties.req).to.have.property("remoteAddress");
+    expect(properties.req).to.have.property("remotePort");
   });
 });
