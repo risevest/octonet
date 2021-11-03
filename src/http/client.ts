ead1a3ecfd93756c4a4f4712aabe549545219ec0
Olakunle Arewa <arewa.olakunle@gmail.com>
2021-11-03 18:00:14 +0100

    feat: add support for tracking timeouts

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-import { APIError, HttpError } from "./errors";
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import { APIError, HttpError, TimeoutError } from "./errors";
 
 export enum HttpMethod {
   GET = "GET",
@@ -26,10 +26,13 @@ export class HttpClient {
   do<T = any>(req: HttpRequest, timeout = 10): Promise<T> {
     return this.instance({ timeout: timeout * 1000, ...req }).then(
       res => res.data,
-      err => {
+      (err: AxiosError) => {
         if (err.response) {
           throw new APIError(err.config.url, err.response.status, err.response.data);
         } else if (err.request) {
+          if (/timeout/.test(err.message)) {
+            throw new TimeoutError(err.config.url, err.config.timeout);
+          }
           throw new HttpError(err.config.url, err);
         } else {
           throw new Error(err.message);
