0a83e5c8a851438e468498a9b4bba72be26aee01
blVck49 <adebiyitope49@gmail.com>
2022-03-24 07:56:07 +0100

    feat: adding prometheus metric service

--- metric.js
+++ metric.js
@@ -1,46 +1,33 @@
-"use strict"
-var __importDefault = (this && this.__importDefault) || 
-function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "_esModule", { value: true});
-const prom_client_1 = __importDefault(require("prom-client"));
-class OctonetMetrics {
-    constructor(config) {
-        this.register = new prom_client_1.default.Registry();
-        this.register.setDefaultLabels({
-            service: config.service_name,
-            environment: config.node_env
-        })
-        prom_client_1.default.collectDefaultMetrics({ register: this.register});
-        this.histogram = new prom_client_1.default.histogram({
-            name: "http_request_response_time",
-            help: "Response time of HTTP requests in seconds",
-            labelNames: ["method", "statusCode", "path"],
-            registers: [this.register]
-        });
-    }
-    /**
-     * HTTP Handler for sending prometheus metrics
-     * @param req Express Request object
-     * @param res Express response object
-     */
-    send(req, res) {
-        res.set("Content-Type", this.register.contentType);
-        res.end(this.register.metrics());
-    }
-    /**
-     * Records a HTTP response
-     * @param req Express request object
-     * @param req Express response object
-     */
-    record(req, res) {
+const client = require("prom-client")
+
+export const octonetMetrics = () => {
+    const register = new client.Registry()
+
+    const histogram = new client.Histogram({
+        name: "http_request_response_time",
+        help: "Response time of HTTP requests",
+        labelNames: ["method", "statusCode", "path"]
+    })
+
+    register.registerMetric(histogram)
+
+    register.setDefaultLabels({
+        app: "octonet-api"
+    })
+
+    client.collectDefaultMetrics({register})
+
+    app.get('/metrics', async(req, res) => {
+        res.setHeader('Content-type', register.contentType)
         const responseTimeHeader = res.getHeader("X-Response-Time");
         const time = parseFloat(responseTimeHeader) / 1000;
         const url = `${req.baseUrl}${req.route.path}`;
-        this.histogram
-            .labels(req.method, String(res.statusCode), url)
-            .observe(time)
-    }
+        histogram.labels(req.method, String(res.statusCode), url).observe(time)
+        res.send(await register.metrics())
+
+    })
+
+
 }
-exports.octonetMetrics = OctonetMetrics
+
+    
