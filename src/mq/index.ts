d97776577c19f41e85dfb9ac2b679cb3f6d394e6
Olakunle Arewa <arewa.olakunle@gmail.com>
2022-03-27 23:18:51 +0100

    ref: make nats available from library index

--- src/mq/index.ts
+++ src/mq/index.ts
@@ -3,3 +3,5 @@ export { AMQPQueue, ChannelProvider, ChannelProviderTag } from "./amqp/queue";
 export { AMQPWorker, command, jobs } from "./amqp/worker";
 export { groupDecorator, handlerDecorator, ParsedHandler, parseHandlers } from "./decorators";
 export * from "./handlers";
+export { NatsConfig, NatsConsumer, stream, subscribe } from "./nats/consumer";
+export { JSClientFactoryTag, JSClientFactory, NatsPublisher, jSClientFactory } from "./nats/publisher";
