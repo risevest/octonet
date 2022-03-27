77fe11e2f5739b511a172e464739038c85c29e8c
blVck49 <adebiyitope49@gmail.com>
2022-03-27 21:39:45 +0100

    fix: added functions to send and record the metrics seperately

--- metric.js
+++ metric.js
@@ -12,22 +12,23 @@ export const octonetMetrics = () => {
     register.registerMetric(histogram)
 
     register.setDefaultLabels({
-        app: "octonet-api"
+        app: "octonet"
     })
 
     client.collectDefaultMetrics({register})
 
-    app.get('/metrics', async(req, res) => {
-        res.setHeader('Content-type', register.contentType)
+    (function send(req, res){
+        res.setHeader("Content-Type", register.contentType);
+        res.send(register.metrics());
+    }())
+
+    (function record(req, res){
         const responseTimeHeader = res.getHeader("X-Response-Time");
         const time = parseFloat(responseTimeHeader) / 1000;
         const url = `${req.baseUrl}${req.route.path}`;
-        histogram.labels(req.method, String(res.statusCode), url).observe(time)
-        res.send(await register.metrics())
-
-    })
-
-
+        histogram.labels(req.method, String(res.statusCode), url).observe(time);
+    }())
+    
 }
 
     
