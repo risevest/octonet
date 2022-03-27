6d6150fa8d4a83ed6e4614748de172d1ac41cad0
Olakunle Arewa <arewa.olakunle@gmail.com>
2022-03-27 23:18:28 +0100

    feat: each publisher should have it's own client

--- src/mq/nats/publisher.ts
+++ src/mq/nats/publisher.ts
@@ -1,15 +1,32 @@
-import { inject } from "inversify";
-import { JSONCodec, JetStreamClient } from "nats";
+import { inject, interfaces } from "inversify";
+import { JSONCodec, JetStreamClient, NatsConnection } from "nats";
 import { v4 as uuidV4 } from "uuid";
 
-export const Jetsream = Symbol.for("JetstreamClient");
+export const JSClientFactoryTag = Symbol.for("JSClientFactory");
+export type JSClientFactory = () => JetStreamClient;
 
 export class NatsPublisher {
-  @inject(Jetsream) private client: JetStreamClient;
+  private client: JetStreamClient;
   private codec = JSONCodec();
+
+  constructor(@inject(JSClientFactoryTag) clientFactory: JSClientFactory) {
+    this.client = clientFactory();
+  }
 
   async publish(subject: string, data: any): Promise<void> {
     const message = this.codec.encode(data);
     await this.client.publish(subject, message, { msgID: uuidV4() });
   }
+}
+
+/**
+ * Connect nats to inversify for the sake of creating jetstream clients
+ * @param conn nats connection
+ */
+export function jSClientFactory(conn: NatsConnection): interfaces.FactoryCreator<JetStreamClient> {
+  return _context => {
+    return () => {
+      return conn.jetstream();
+    };
+  };
 }
