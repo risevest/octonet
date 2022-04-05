437e930bcdf6133cf9309969e2777ebe7b48bf01
blVck49 <adebiyitope49@gmail.com>
2022-04-05 07:25:55 +0100

    chore: class name changed to WebMetrics

--- metric.ts
+++ metric.ts
@@ -1,30 +1,22 @@
 import { Request, Response } from "express";
 import client from "prom-client";
 
-export class octonetMetrics {
+export class WebMetrics {
     private histogram: any;
     register: any;
-
-    constructor() {
-
+    constructor(config: any) {
         this.register = new client.Registry()
-
         this.histogram = new client.Histogram({
             name: "http_request_response_time",
             help: "Response time of HTTP requests",
             labelNames: ["method", "statusCode", "path"]
         })
-
         this.register.registerMetric(this.histogram)
-
         this.register.setDefaultLabels({
-            app: "octonet"
+            app: config.app_name
         })
-
         client.collectDefaultMetrics({register: this.register})
-
     }
-
     /**
      * HTTP Handler for sending prometheus metrics
      * @param req Express Request object
@@ -34,7 +26,6 @@ export class octonetMetrics {
         res.setHeader("Content-Type", this.register.contentType);
         res.send(this.register.metrics)
     }
-    
     /**
      * Records a HTTP response
      * @param req Express request object
