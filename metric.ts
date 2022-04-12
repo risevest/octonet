4dbd1f5bea1c16a7a867547d6b5d3176028a44d0
blVck49 <adebiyitope49@gmail.com>
2022-04-12 13:49:56 +0100

    chore: modified class Webmetrics

--- metric.ts
+++ metric.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import client, { Histogram, Registry } from "prom-client";
+import { Histogram, Registry, collectDefaultMetrics } from "prom-client";
 import { AppConfig } from "./env";
 
 export class WebMetrics {
     private histogram: Histogram<string>;
-    public register: Registry;
+    private register: Registry;
     
     constructor(config: AppConfig) {
         this.register = new Registry()
@@ -17,7 +17,7 @@ export class WebMetrics {
         this.register.setDefaultLabels({
             app: config.app_name
         })
-        client.collectDefaultMetrics({register: this.register})
+        collectDefaultMetrics({register: this.register})
     }
     /**
      * HTTP Handler for sending prometheus metrics
