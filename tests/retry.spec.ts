a397719bd7e594005df758f619ffc79de517545c
Olakunle Arewa <arewa.olakunle@gmail.com>
2022-08-12 02:41:49 +0100

    test: add retry module tests

---
+++ tests/retry.spec.ts
@@ -0,0 +1,126 @@
+import { expect } from "chai";
+
+import { ExitError, RetryError, retryOnError, retryOnRequest } from "../src/retry";
+
+describe("retryOnError", () => {
+  it("should not retry when maxRetries is 0", async () => {
+    let count = 0;
+
+    try {
+      await retryOnError(0, "10ms", async () => {
+        ++count;
+
+        throw new Error("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(1);
+  });
+
+  it("should retry according to maxRetries", async () => {
+    let count = 0;
+
+    try {
+      await retryOnError(4, "10ms", async () => {
+        ++count;
+
+        throw new Error("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(5);
+  });
+
+  it("should exit when no error", async () => {
+    let count = 0;
+
+    try {
+      await retryOnError(4, "10ms", async () => {
+        ++count;
+
+        if (count === 2) {
+          return;
+        }
+
+        throw new Error("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(2);
+  });
+
+  it("should space retries using exponential backoff", async () => {
+    let timeouts: number[] = [];
+    let now = new Date().getTime();
+
+    try {
+      await retryOnError(2, "10ms", async () => {
+        const temp = now;
+        now = new Date().getTime();
+        timeouts.push(now - temp);
+
+        throw new Error("error");
+      });
+    } catch (error) {}
+
+    expect(timeouts).to.have.length(3);
+    timeouts.slice(1).forEach((t, i) => {
+      const expectedT = 10 * Math.pow(2, i);
+
+      // we give an error of 5 extra ms
+      expect(t).to.be.within(expectedT, expectedT + 5);
+    });
+  });
+
+  it("should exit early", async () => {
+    let count = 0;
+
+    try {
+      await retryOnError(4, "10ms", async () => {
+        ++count;
+
+        if (count === 3) {
+          throw new ExitError();
+        }
+
+        throw new Error("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(3);
+  });
+});
+
+describe("retryOnRequest", () => {
+  it("should try only one attempt by default", async () => {
+    let count = 0;
+
+    try {
+      await retryOnRequest(4, "10ms", async () => {
+        ++count;
+
+        throw new Error("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(1);
+  });
+
+  it("should retry on RetryError", async () => {
+    let count = 0;
+
+    try {
+      await retryOnRequest(4, "10ms", async () => {
+        ++count;
+
+        if (count === 3) {
+          throw new Error();
+        }
+
+        throw new RetryError("error");
+      });
+    } catch (err) {}
+
+    expect(count).to.eq(3);
+  });
+});
