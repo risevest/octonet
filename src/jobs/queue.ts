c9000253aa217524f05186f721804f7816977884
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2022-08-19 23:47:34 +0100

    feat: skip for redis queue

--- src/jobs/queue.ts
+++ src/jobs/queue.ts
@@ -1,9 +1,12 @@
 import { Redis } from "ioredis";
 import { injectable } from "inversify";
 
+const queueAction = <const>["processed", "skipped", "requeued"];
+export type QueueAction = typeof queueAction[number];
+
 @injectable()
 export class RedisQueue<T> {
-  constructor(private name: string, private redis: Redis) {}
+  constructor(private name: string, private redis: Redis, private maxRequeues = 2) {}
 
   async fill(ts: T[]) {
     const length = await this.length();
@@ -12,16 +15,32 @@ export class RedisQueue<T> {
     }
 
     const instructions = ts.map(t => ["rpush", this.name, JSON.stringify(t)]);
-    return await this.redis.multi(instructions).exec();
+    await this.redis.multi(instructions).exec();
+    this.maxRequeues = ts.length;
   }
 
   async length() {
     return await this.redis.llen(this.name);
   }
 
-  async work(f: (t: T) => Promise<void>) {
+  async skip() {
+    return await this.redis.lpop(this.name);
+  }
+
+  async requeue() {
+    const item = await this.skip();
+    return await this.redis.rpush(this.name, item);
+  }
+
+  async work(f: (t: T, skip: () => Promise<string>, requeue: () => Promise<number>) => Promise<QueueAction>) {
     const length = await this.length();
+    let retries = 0;
+
     for (let index = 0; index < length; index++) {
+      if (retries > this.maxRequeues) {
+        return;
+      }
+
       const entries = await this.redis.lrange(this.name, 0, 0);
 
       // first empty entry is sign we should skip
@@ -29,10 +48,23 @@ export class RedisQueue<T> {
         return;
       }
 
-      await f(JSON.parse(entries[0]));
+      const action = await f(JSON.parse(entries[0]), this.skip, this.requeue);
+      switch (action) {
+        case "processed":
+          // clean it off
+          await this.redis.lpop(this.name);
+          break;
+        case "skipped":
+          continue;
+        case "requeued":
+          // adds an additional loop
+          index -= 1;
+          // prevents an infinite loop
+          retries += 1;
+          break;
+      }
 
-      // clean it off
-      await this.redis.lpop(this.name);
+      // await this.redis.lpop(this.name);
     }
   }
 }
