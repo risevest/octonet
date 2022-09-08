9344add2db9ab3e758cf4592cb64b9a94b94da55
Olakunle Arewa <arewa.olakunle@gmail.com>
2022-09-08 01:31:31 +0100

    fix: forgot to add index to git

--- src/mq/index.ts
+++ src/mq/index.ts
@@ -3,7 +3,7 @@ export * from "./amqp/queue";
 export { command, worker, Workers } from "./amqp/workers";
 export { groupDecorator, handlerDecorator, ParsedHandler, parseHandlers } from "./decorators";
 export * from "./handlers";
-export { Consumers, NatsConfig, stream, subscribe } from "./nats/consumer";
+export { Consumers, NatsConfig, stream, subscribe } from "./nats/consumers";
 export { StreamConfig, StreamFactory } from "./nats/factory";
 export { NatsPublisher } from "./nats/publisher";
 export { Stream } from "./nats/stream";
